cdd1f5665689a6a8bbe960a144f44a6fe766222d
amarjeetsurana-boop <amarjeetsurana@gmail.com>
2026-06-23 00:30:04 +0530

    Remove cache implementation and add start_url

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-const CACHE_NAME = 'tgc9-v1';
-const ASSETS = [
-  'index.html',
-  'img/Logo.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+{ "start_url": "public/index.html" }
